7abc09dea6d3a7457047c62bdf5082ed719f48be
Yurii Tsykvas <tsykvasyurii@gmail.com>
2023-11-05 20:26:22 +0200

    Fixed registration with additional params.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   root to: "pages#home"
 
   get '/change_locale_to_en', to: 'application#change_locale_to_en', as: :change_locale_to_en
