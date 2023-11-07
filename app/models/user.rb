2b187a06def51b60e28b68bcd3d49c3485ea3470
Yurii Tsykvas <tsykvasyurii@gmail.com>
2023-11-07 19:24:50 +0200

    Validation for user_type and company_name fields

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :user_type, presence: true, inclusion: { in: %w[buyer supplier] }
+  validates :company_name, presence: true
 end
