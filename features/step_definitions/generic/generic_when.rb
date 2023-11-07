efda90fc7577e0896c44a561c043fb056c301a85
Yurii Tsykvas <tsykvasyurii@gmail.com>
2023-11-07 19:47:45 +0200

    Cucumber test for registration

---
+++ features/step_definitions/generic/generic_when.rb
@@ -0,0 +1,68 @@
+When(/I visit (.*?) page/) do |page|
+  path = case page
+         when 'sign up'
+           new_user_registration_path
+         when 'log in'
+           new_user_session_path
+         when 'positions'
+           positions_path
+         when 'new position'
+           new_position_path
+         when 'edit position'
+           edit_position_path
+         end
+  visit path
+end
+
+When(/I fill "(.*?)" in the (.*?) (field|select box)/) do |text, field_test_id, type|
+  hash_of_data_test_id = {
+    # Devise
+    email: 'email-input',
+    password: 'password-input',
+    'password confirmation': 'password_confirmation-input',
+    'user type': 'user-type-input',
+    'company name': 'company-name-input',
+    # Position
+    'position title': 'title_field',
+    'position body': 'body_field',
+    'position experience': 'experience_field'
+  }.stringify_keys
+
+  input_element = find("[data-test-id='#{hash_of_data_test_id[field_test_id.downcase]}']")
+  if type == 'field'
+    input_element.fill_in(with: text)
+  elsif type == 'select box'
+    input_element.select(text)
+  end
+end
+
+When (/I click on the (.*?) (link|button)/) do |test_id, type|
+  link_test_id = {
+    # dashboard
+  }
+  button_test_id = {
+    'submit': 'submit_button',
+    # dashboard
+    'avatar': 'avatar_picture',
+    'sign out': 'log_out_button',
+    # position
+    'create position': 'create_position_btn',
+    'edit position': 'edit_position_btn',
+    # candidate
+    'show candidates': 'show_candidates_btn',
+    'show position': 'show_position_btn'
+  }.stringify_keys
+  object = case type
+           when "link"
+             link_test_id[test_id.downcase]
+           when "button"
+             button_test_id["#{test_id.downcase}"]
+           else
+             "unknown test id"
+           end
+  find("[data-test-id='#{object}']").click
+end
+
+When(/^I submit the form$/) do
+  find('input[type="submit"]').click
+end
---
+++ features/user_stories/user/register.feature
@@ -0,0 +1,22 @@
+Feature: User should be able create a new account
+
+  Scenario: User can register new account with mail and password
+    When I visit sign up page
+    Then I should see the text: sign up title
+    And I fill "example@mail.com" in the email field
+    And I fill "password123" in the password field
+    And I fill "password123" in the password confirmation field
+    And I fill "Надаю товари продавцям" in the user type select box
+    And I fill "Test Company" in the company name field
+    And I submit the form
+    And I should not see the text: sign up title
+    And I should see the text: success registration alert
+
+  Scenario: User will get an error when leave one of the fields empty
+    When I visit sign up page
+    Then I should see the text: sign up title
+    And I fill "example@mail.com" in the email field
+    And I fill "Надаю товари продавцям" in the user type select box
+    And I submit the form
+    And I should not see the text: success registration alert
+    And I should see the text: sign up title
